8d8e4ccaa368dba2f052d34e690100fbb365f342
Juan Nuñez <tojpnunez@gmail.com>
2020-12-08 18:30:50 -0300

    single page application part 2

--- src/app/dish-detail/dish-detail.component.ts
+++ src/app/dish-detail/dish-detail.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { isProtractorLocator } from 'protractor/built/locators';
 import { Dish } from '../shared/dish';
-
-
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { DishService } from "../services/dish.service";
 
 @Component({
   selector: 'app-dish-detail',
@@ -18,9 +19,16 @@ export class DishDetailComponent implements OnInit {
 
   
 
-  constructor() { }
+  constructor( private dishService: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit(): void {
+    let id = this.route.snapshot.params['id'];
+    this.dish = this.dishService.getDish(id);
   }
 
+  goBack(): void {
+    this.location.back();
+  }
 }
